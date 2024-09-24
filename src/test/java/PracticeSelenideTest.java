c456c8266619c782d40a3c3d8014147de8a5acf4
Anna <jenova.anya@yandex.ru>
2024-09-25 00:11:00 +0300

    added PracticeSelenideTest

--- src/test/java/PracticeSelenideTest.java
+++ src/test/java/PracticeSelenideTest.java
@@ -2,12 +2,32 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class PracticeSelenideTest {
     @BeforeAll
     static void beforeAll() {
+        Configuration.browserSize = "1920*1080";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://github.com";
+        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
 
+
+    }
+    @Test
+    void checkJUnit () {
+        // - Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+        // - Перейдите в раздел Wiki проект
+        $("#wiki-tab").click();
+        // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-content").$(byText("Show 3 more pages…")).click();
+        $("#wiki-wrapper").shouldHave(text("SoftAssertions"));
+        // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(byText("SoftAssertions")).click();
     }
 }
